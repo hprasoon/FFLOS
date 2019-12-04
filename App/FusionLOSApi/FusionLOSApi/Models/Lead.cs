65ed9e774e7ea5e8ed4bd98b274a29eeed12866e
Hariprasoon Ramachandran <Hariprasoon.Ramachandran@dh.com>
2019-12-05 00:18:00 +0530

    Api changes

--- App/FusionLOSApi/FusionLOSApi/Models/Lead.cs
+++ App/FusionLOSApi/FusionLOSApi/Models/Lead.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static CallRequestResponseService.MLSERVICE_CALL;
 
 namespace FusionLOS.Models
 {
@@ -22,6 +27,66 @@ namespace FusionLOS.Models
         }
 
         public void Add(Lead newloan)
+        {
+            //FFDC
+            FFDCSave(newloan);
+
+            LocalSave(newloan);
+        }
+
+        private void FFDCSave(Lead newloan)
+        {
+            CreateFFDCLoan(newloan);
+        }
+
+        private static async void CreateFFDCLoan(Lead newloan)
+        {
+            using (var client = new HttpClient())
+            {
+                var Tockenclient = new HttpClient();
+                Token tok = await GetEligilityToken(Tockenclient);
+                Tockenclient.Dispose();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tok.AccessToken);
+
+                client.BaseAddress = new Uri("https://api.preprod.fusionfabric.cloud/mortgagebot/los/lead-import/v1/loans");
+                HttpResponseMessage response = await client.GetAsync("");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    JObject joResponse = JObject.Parse(result);
+                }
+            }
+        }
+
+        private static async Task<Token> GetEligilityToken(HttpClient client)
+        {
+            try
+            {
+                string baseAddress = @"https://api.preprod.fusionfabric.cloud/login/v1/sandbox/oidc/token";
+                string grant_type = "client_credentials";
+                string client_id = "1eb8d602-2e53-403a-8e61-f760dc1ac6c8";
+                string client_secret = "e99b9f2b-eb59-4f24-aabe-a2e53b73f910";
+
+                var form = new Dictionary<string, string>
+                {
+                    {"grant_type", grant_type},
+                    {"client_id", client_id},
+                    {"client_secret", client_secret},
+                };
+
+                HttpResponseMessage tokenResponse = await client.PostAsync(baseAddress, new FormUrlEncodedContent(form));
+                var jsonContent = await tokenResponse.Content.ReadAsStringAsync();
+                Token tok = JsonConvert.DeserializeObject<Token>(jsonContent);
+                return tok;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void LocalSave(Lead newloan)
         {
             if (newloan == null)
             {
