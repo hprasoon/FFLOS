193528b4b9217f879eb92add6a37f09bbcbdb2a1
Hariprasoon Ramachandran <Hariprasoon.Ramachandran@dh.com>
2019-12-04 19:54:54 +0530

    Api changes

---
+++ App/FusionLOSApi/FusionLOSApi/Controllers/InstantEligibilityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using CallRequestResponseService;
+
+namespace FusionLOS.Controllers
+{
+    public class InstantEligibilityController : ApiController
+    {
+        public async Task<string> InstantEligibilityCheck([FromBody]string customerId)
+        {
+            return await RequestResponseService(Convert.ToInt32(customerId));
+        }
+
+        public async Task<string> RequestResponseService(int consumerid)
+        {
+            var eligibilityparams = await MLSERVICE_CALL.GetFFDCDataForInitialCheck(consumerid);
+            return await MLSERVICE_CALL.InvokeInitialCheckService(eligibilityparams).ConfigureAwait(false);
+        }
+    }
+}
